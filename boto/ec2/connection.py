b5e0d823e0d83b746e81ee347fbbcda29c7b5456
Vasily Sovetov <vsovetov@croc.ru>
2024-07-14 18:29:54 +0300

    fixup! ec2: Add RamMonitoring Instance attribute

--- boto/ec2/connection.py
+++ boto/ec2/connection.py
@@ -1378,9 +1378,12 @@ class EC2Connection(AWSQueryConnection):
         :return: Whether the operation succeeded or not
         """
         # Allow a bool to be passed in for value of disableApiTermination
-        bool_reqs = ('disableapitermination',
-                     'sourcedestcheck',
-                     'ebsoptimized')
+        bool_reqs = (
+            'disableapitermination',
+            'sourcedestcheck',
+            'ebsoptimized',
+            'rammonitoring',
+        )
         if attribute.lower() in bool_reqs:
             if isinstance(value, bool):
                 if value:
